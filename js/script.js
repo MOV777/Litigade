2255b6b2e0edd1afdea55c01ebf8d55f7ee2a82d
Oleksandr Minenko <minenkogt@gmail.com>
2022-08-08 14:17:21 +0300

    header with mobile adaptation is done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let btn = document.querySelector('.nav__toggle');
 let navigation = document.querySelector('.nav');
 let header_top = document.querySelector('.header__top');
 let menu = document.querySelector('.menu');
+let header_bottom = document.querySelector('.header__bottom');
 btn.onclick = function () {
-   header_top.classList.toggle('header__top--light');
    navigation.classList.toggle('nav--show');
    menu.classList.toggle('menu--show');
 }
