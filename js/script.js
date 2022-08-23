d8965975b3a3cf228f2583d852fcd32e48736213
Oleksandr Minenko <minenkogt@gmail.com>
2022-08-23 17:58:20 +0300

    popup for video is done and all changes is saved

--- js/script.js
+++ js/script.js
@@ -7,4 +7,15 @@ btn.onclick = function () {
    navigation.classList.toggle('nav--show');
    menu.classList.toggle('menu--show');
 }
+jQuery(function ($) {
+   if ($('.video__btn').length) {
+      $('.video__btn').magnificPopup({
+         disableOn: 700,
+         type: 'iframe',
+         mainClass: 'mfp-fade',
+         removalDelay: 160,
 
+         fixedContentPos: false,
+      });
+   }
+});
